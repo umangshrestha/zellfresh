8e47993b8edb6da3f3681eabf9b7ea96a9b25ef5
shrestu <umang.shrestha@autodesk.com>
2024-11-28 16:21:28 -0500

    removed unecessary dev dependencies

--- infrastructure/dynamodb.tf
+++ infrastructure/dynamodb.tf
@@ -105,18 +105,3 @@ resource "aws_dynamodb_table" "reviews_table" {
     Project = local.project_name
   }
 }
-
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Project = local.project_name
-  }
-}
